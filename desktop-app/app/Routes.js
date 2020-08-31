4430c4a75ac585beff3e2f286bdcb6db04f3cc01
Johnny Zabala <jzabala.s@gmail.com>
2020-08-30 21:41:01 -0400

    feat(desktop-app): add light theme

--- desktop-app/app/Routes.js
+++ desktop-app/app/Routes.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import {Switch, Route} from 'react-router';
+import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
+import {makeStyles} from '@material-ui/core/styles';
 import routes from './constants/routes';
-import App from './containers/App';
 import Browser from './containers/Browser';
 import LeftIconsPaneContainer from './containers/LeftIconsPaneContainer';
 import styles from './layout.css';
 import StatusBarContainer from './containers/StatusBarContainer';
 import DevToolResizerContainer from './containers/DevToolResizerContainer';
 
-export default () => (
-  <App>
-    <div className={styles.appRoot}>
-      <div className={styles.iconColumn}>
-        <LeftIconsPaneContainer />
+const Routes = () => {
+  const classes = useStyles();
+  return (
+    <Paper className={classes.root}>
+      <div className={classes.main}>
+        <div className={styles.iconColumn}>
+          <LeftIconsPaneContainer />
+        </div>
+        <div className={styles.contentColumn}>
+          <Switch>
+            <Route path={routes.HOME} component={Browser} />
+          </Switch>
+        </div>
       </div>
-      <div className={styles.contentColumn}>
-        <Switch>
-          <Route path={routes.HOME} component={Browser} />
-        </Switch>
-      </div>
-    </div>
-    <StatusBarContainer />
-    <DevToolResizerContainer />
-  </App>
-);
+      <StatusBarContainer />
+      <DevToolResizerContainer />
+    </Paper>
+  );
+};
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+    backgroundColor: theme.palette.background.default,
+  },
+  main: {
+    display: 'flex',
+    height: '100%',
+    backgroundColor: 'transparent',
+  },
+}));
+
+export default Routes;
